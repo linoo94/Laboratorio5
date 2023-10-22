6a342b1fccb906682ba3142d516e3f7408a6f064
linoo94 <134905646+linoo94@users.noreply.github.com>
2023-10-22 00:17:41 -0600

    Add files via upload

--- servidor.php
+++ servidor.php
@@ -1,59 +1,56 @@
+
 <?php
-/*conexion a la base de dato*/
+/* Función para conectar utilizando PDO */
+function conectar() {
+    $servidor = "127.0.0.1";
+    $usuario = "root";
+    $password = "";
+    $database = "chat";
 
-function conectar($servidor = "127.0.0.1", $usuario = "root", $password = "", $database = "chat") {
-    $conexion = new mysqli($servidor, $usuario, $password, $database);
-
-    if ($conexion->connect_error) {
-        die("Error de conexión: " . $conexion->connect_error);
+    try {
+        $conexion = new PDO("mysql:host=$servidor;dbname=$database", $usuario, $password);
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conexion;
+    } catch (PDOException $e) {
+        die("Error de conexión: " . $e->getMessage());
     }
-
-    // Configura el juego de caracteres para UTF-8, si es necesario
-    $conexion->set_charset("utf8");
-
-    return $conexion;
 }
 
-/*funcion para ejecutar*/
-function ejecutar($sql){
-	$conexion = conectar();
-	mysqli_query($conexion, $sql);
+/* Función para ejecutar una consulta utilizando PDO */
+function ejecutar($sql) {
+    $conexion = conectar();
+    
+    try {
+        $conexion->exec($sql);
+    } catch (PDOException $e) {
+        die("Error en la consulta: " . $e->getMessage());
+    }
 }
-/*funcion para consultar*/
-function consultar($sql,$cols_num){
-	$conexion = conectar();
-	$query = $conexion->query($sql);
-	$matriz = array();
-	$f = 0;
-	while($celda = $query->fetch_assoc()){
-		$keys = array_keys($celda);
-		for($c=0;$c<$cols_num;$c++){$matriz[$f][$c]=$celda[$keys[$c]];}
-		$f++;
-	}
-	return $matriz;
+
+/* Función para consultar utilizando PDO */
+function consultar($sql, $cols_num) {
+    $conexion = conectar();
+    
+    try {
+        $query = $conexion->query($sql);
+        $matriz = $query->fetchAll(PDO::FETCH_NUM);
+        return $matriz;
+    } catch (PDOException $e) {
+        die("Error en la consulta: " . $e->getMessage());
+    }
 }
-/*funcion de retorno de datos AJAX*/
-function AJAX($nombre, $mensaje){
-	if(($nombre!="")&&($mensaje!="")){
-		ejecutar("CALL insertar('".$nombre."','".$mensaje."');");
-	}
-	$chat = consultar("CALL mostrar();",2);
-	header("Content-Type: text/plain");
-	foreach ($chat as $dato){
-		echo($dato[0].": ".$dato[1]."\n\n");
-	}
-}
-/*solo si recive variable nombre y mensaje sabemos es el AJAX*/
-if(isset($_REQUEST["nombre"])&&isset($_REQUEST["mensaje"])){
-	$nombre = $_REQUEST["nombre"];
-	$mensaje = $_REQUEST["mensaje"];
-//$fp = fopen('archivo.txt','a');
-//fwrite($fp,$mensaje."\n");
-//fclose($fp);
-	AJAX($nombre, $mensaje);
-}
-/*si intenta ingresar sin autorizacion*/
-else{
-	echo("Solo Personal Autorizado");
+
+/* Resto de tu código sigue igual */
+
+/* Función de retorno de datos AJAX */
+function AJAX($nombre, $mensaje) {
+    if ($nombre != "" && $mensaje != "") {
+        ejecutar("CALL insertar('".$nombre."','".$mensaje."')");
+    }
+    $chat = consultar("CALL mostrar()", 2);
+    header("Content-Type: text/plain");
+    foreach ($chat as $dato) {
+        echo($dato[0].": ".$dato[1]."\n\n");
+    }
 }
 ?>
